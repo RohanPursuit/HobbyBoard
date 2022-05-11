bce86f2704f23f57dd9569a283434a22beb90a38
BennyZ3 <bennyz3@hotmail.com>
2022-05-11 11:21:36 -0400

    updated db seed and schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS study_dev;
-CREATE DATABASE study_dev;
+DROP DATABASE IF EXISTS hobbyboard_dev;
+CREATE DATABASE hobbyboard_dev;
 
-\c study_dev;
+\c hobbyboard_dev;
 
-DROP TABLE IF EXISTS groups, events, users;
+DROP TABLE IF EXISTS groups, users;
 
 CREATE TABLE groups (
     name TEXT PRIMARY KEY,
@@ -12,16 +12,6 @@ CREATE TABLE groups (
     contact TEXT
 );
 
-CREATE TABLE events (
-    name TEXT NOT NULL,
-    virtual_meeting_link TEXT,
-    study_group TEXT,
-    start_time TIMESTAMP,
-    end_time TIMESTAMP,
-    number_of_attendees INT,
-    cancelled BOOLEAN,
-    FOREIGN KEY (study_group) REFERENCES groups(name)
-);
 
 CREATE TABLE users (
     username TEXT NOT NULL,
