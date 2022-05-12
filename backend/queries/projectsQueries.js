64febff11fb6c9ea42230eabc6d13704f0b761d2
DylanLovell93 <arcanyne7@gmail.com>
2022-05-12 16:42:30 -0400

    created getAllProjects query gunction

---
+++ backend/queries/projectsQueries.js
@@ -0,0 +1,21 @@
+//import the DB variable that we'll be making queries to
+const db = require("../db/dbConfig");
+
+//getAllProjects async function
+//input(none)
+//output array of all projects
+const getAllProjects = async () => {
+  //try to fetch all items from projects table
+  try {
+    const allProjects = await db.any("SELECT * FROM projects");
+    //return it
+    console.log(allProjects);
+    return allProjects;
+  } catch (err) {
+    //if there's an error, return it.
+    return err;
+  }
+};
+
+//export query functions
+module.exports = { getAllProjects };
