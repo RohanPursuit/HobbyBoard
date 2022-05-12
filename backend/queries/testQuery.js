1ce0123c74599ae34275f25e05f1884e735d6fee
DylanLovell93 <arcanyne7@gmail.com>
2022-05-12 12:35:00 -0400

    created testQuery function

---
+++ backend/queries/testQuery.js
@@ -0,0 +1,21 @@
+//import the DB variable that we'll be making queries to
+const db = require("../db/dbConfig");
+
+//Create a function that returns a database query
+// async, since it contains a db call
+// try the query,
+const testQuery = async () => {
+  //try the query
+  try {
+    //fetch data from the database
+    const test = db.one('SELECT * FROM groups WHERE contact="Benny"');
+    //return it
+    return test;
+  } catch (err) {
+    //if the query fails, returns the error
+    return err;
+  }
+};
+
+//export function
+module.exports = { testQuery };
