b36f74afd7379dccf1c6420ffc9f50e224dcdff5
justin <justin.d.diaz@gmail.com>
2022-05-12 17:48:34 -0400

    app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 //IMPORT QUERIES
 const { testQuery } = require("./queries/testQuery");
+const projectsControllers = require("./controllers/projectControllers");
 
 // CONFIGURATION
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
 // ROUTES
+app.use("/projects", projectsControllers);
 //async, so we can use query correctly
 app.get("/", async (req, res) => {
   //use query function to get data from db
