58b5c5923c5b298a2423e4720a7bd8ee384634bd
BennyZ3 <bennyz3@hotmail.com>
2022-05-13 11:31:30 -0400

    added AllPorjects pade

---
+++ frontend/src/pages/AllProjects.js
@@ -0,0 +1,11 @@
+import Projects from "../components/Projects"
+
+const AllProjects = () => {
+    return (
+      <div className="AllProjects">
+        <Projects />
+      </div>
+    );
+}
+
+export default AllProjects;
