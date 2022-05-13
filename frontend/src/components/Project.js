036307d6954923189967bc9a08d0820f2bc4a5a0
DylanLovell93 <arcanyne7@gmail.com>
2022-05-13 14:35:12 -0400

    v1 styles

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./Project.css";
 
 const Project = (props) => {
-    return (
-      <div className="Project">
-        <img src={props.project_image} alt='' width='75' height='75'/>
-        <h3>{props.details.name}</h3>
-        <p>{props.details.details}</p>
-      </div>
-    );
-}
+  return (
+    <div className="Project">
+      <img src={props.project_image} alt="" width="75" height="75" />
+      <h3>{props.details.name}</h3>
+      <p>{props.details.details}</p>
+    </div>
+  );
+};
 
 export default Project;
