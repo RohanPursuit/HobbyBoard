2e164d3dd8641d9281ddbd88d865acd8905fb2af
RohanPursuit <rohansterling@pursuit.org>
2022-05-13 12:18:24 -0400

    replace Link with useNavigate imported from react-router-dom

--- frontend/src/components/common/NavBar.js
+++ frontend/src/components/common/NavBar.js
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import plus from "../../assets/circle-plus.svg";
 
 const NavBar = () => {
+
+
   return (
     <nav className="Nav">
       <img src={plus} alt="Add Project" />
