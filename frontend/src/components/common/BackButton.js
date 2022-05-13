e93aa907378a6ed961cf9c8c5b97c6f2b54bb9ad
RohanPursuit <rohansterling@pursuit.org>
2022-05-13 15:51:21 -0400

    add useNavigate to go to previous page on a click event

--- frontend/src/components/common/BackButton.js
+++ frontend/src/components/common/BackButton.js
@@ -1,8 +1,10 @@
+import {useNavigate} from 'react-router-dom'
 import backButton from "../../assets/arrow-back-circle.svg"
 
 function BackButton(){
+    const history = useNavigate()
     return(
-        <img src={backButton} alt=""/>
+        <img onClick={()=> history(-1)} className="back-button" src={backButton} alt="Go back one page"/>
     )
 }
 
