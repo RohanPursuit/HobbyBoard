4a9d68b46ae6a4efdc00f65e674a012746e47cf6
BennyZ3 <bennyz3@hotmail.com>
2022-05-13 21:21:21 -0400

    added user route to app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const express = require("express");
 //IMPORT QUERIES
 const { testQuery } = require("./queries/testQuery");
 const projectsControllers = require("./controllers/projectControllers");
+const userController = require("./controllers/userController");
 
 // CONFIGURATION
 const app = express();
@@ -15,6 +16,8 @@ app.use(express.json()); // Parse incoming JSON
 
 // ROUTES
 app.use("/projects", projectsControllers);
+app.use("/users", userController);
+
 //async, so we can use query correctly
 app.get("/", async (req, res) => {
   //use query function to get data from db
