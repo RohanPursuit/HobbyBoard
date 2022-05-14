9208e65cc863f7f1bc6413e48311f422bdff441d
BennyZ3 <bennyz3@hotmail.com>
2022-05-13 21:06:11 -0400

    added query for inserting a new user

---
+++ backend/queries/userQueries.js
@@ -0,0 +1,25 @@
+//Access the database that will be used
+const db = require("../db/dbConfig");
+
+// addNewUser async function
+// input: user object containing [username, password, email, date, details]
+// output: the newly created user as an object
+const addNewUser = async (user) => {
+  // destructure to get individual parameters
+  let { username, password, email, date, details } = user;
+  try {
+    //attempt to insert into the users table
+    const newUser = await db.one(
+      "INSERT INTO users (username, password, email, date, details) VALUES ($1,$2,$3,$4,$5) RETURNING *",
+      [username, password, email, date, details]
+    );
+    //return resulting user object
+    return newUser;
+  } catch (error) {
+    //catch in case of failure
+    //most likely cause should be attempting to use an existing primary key(username)
+    return error;
+  }
+};
+
+module.exports = { addNewUser };
