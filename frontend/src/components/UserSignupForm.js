3ea120b1d2a7544392332ce80b730e5be6161d49
RohanPursuit <rohansterling@pursuit.org>
2022-05-16 16:09:37 -0400

    add confirm-password state, if both password fields are not the same, an alert message will be shown to the user.

--- frontend/src/components/UserSignupForm.js
+++ frontend/src/components/UserSignupForm.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 function UserSignupForm(){
     const API = process.env.REACT_APP_API_URL;
     const nav = useNavigate()
+    const [confirmPassword, setConfirmPassword] = useState("")
     const [chooseUserName, setChooseUserName] = useState(false)
     const [userInfo, setUserInfo] = useState({
         email: "",
@@ -13,19 +14,26 @@ function UserSignupForm(){
     })
 
     const handleInputChange = (event) => {
+        if(event.target.id === "confirm-password"){
+            setConfirmPassword(event.target.value)
+            return;
+        }
         setUserInfo({...userInfo, [event.target.name]: event.target.value})
     }
 
     const inputUserName = (event) => {
         event.preventDefault()
-        setChooseUserName(true)
+        if(confirmPassword === userInfo.password){
+            setChooseUserName(true)
+        } else {
+            alert("Password doesn't match")
+        }
     }
 
     const handleSubmit = (event) => {
         event.preventDefault()
         const today = new Date
         const [year, month, day] = [today.getFullYear(), today.getMonth(), today.getDay()]
-        
         //axios post userInfo
         axios.post(API + "users/", {
             ...userInfo,
@@ -37,7 +45,6 @@ function UserSignupForm(){
         .catch(console.log)
     }
 
-    console.log(userInfo)
     return (
         <div className="UserSignupForm">
             <form onChange={handleInputChange}>
@@ -54,7 +61,7 @@ function UserSignupForm(){
                     <input id="email" name="email" type="text" required />
                     <label htmlFor="password">Password</label>
                     <input id="password" name="password" type="text" />
-                    <label htmlFor="confirm-password">Password</label>
+                    <label htmlFor="confirm-password">Confirm Password</label>
                     <input id="confirm-password" name="password" type="text" />
                     <input onClick={inputUserName} type="submit"/>
                 </>
