359fc3d095224cf921467e45b9fae9d606fb64a2
RohanPursuit <rohansterling@pursuit.org>
2022-05-16 15:23:43 -0400

    add form with username, password inputs and a input with type submit

--- frontend/src/components/SignInForm.js
+++ frontend/src/components/SignInForm.js
@@ -1,7 +1,14 @@
 function SignInForm(){
     return (
         <div>
-
+            <form >
+                <label htmlFor="username">Username</label>
+                <input id="username" name="username" type="text" />
+                <label htmlFor="password">Password</label>
+                <input id="password" name="password" type="text" />
+                <div>Forgot password</div>
+                <input type="submit" />
+            </form>
         </div>
     )
 }
