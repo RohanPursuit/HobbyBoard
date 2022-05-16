eb3c297bb0f3172ae230a24e1a883e3b7999360e
RohanPursuit <rohansterling@pursuit.org>
2022-05-16 17:55:47 -0400

    remove forgot password placeholder text

--- frontend/src/components/UserSignInForm.js
+++ frontend/src/components/UserSignInForm.js
@@ -24,7 +24,6 @@ function UserSignInForm(){
                 <input id="username" name="username" type="text" />
                 <label htmlFor="password">Password</label>
                 <input id="password" name="password" type="text" />
-                <div>Forgot password</div>
                 <input type="submit" />
                 <Link to="/signup">
                     Sign Up
