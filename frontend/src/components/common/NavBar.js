f2a99374170969ab7c55a2d040cacb6a2f3a248e
justin <justin.d.diaz@gmail.com>
2022-05-16 16:52:43 -0400

    nav

--- frontend/src/components/common/NavBar.js
+++ frontend/src/components/common/NavBar.js
@@ -1,17 +1,29 @@
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import plus from "../../assets/circle-plus.svg";
+import profile from "../../assets/profile-circled.svg";
+import settings from "../../assets/settings.svg";
+import signIn from "../../assets/sign-in.svg";
+import home from "../../assets/home.svg";
+import "./NavBar.css";
 
 const NavBar = () => {
-  const nav = useNavigate()
-  const location = useLocation().pathname
+  const nav = useNavigate();
 
   const handleCreateProject = () => {
-    nav("/projects/new")
-  }
+    nav("/projects/new");
+  };
+
+  const handleSignIn = () => {
+    nav("projects/signIn");
+  };
 
   return (
     <nav className="Nav">
-      {location !== '/projects/new' && <img onClick={handleCreateProject} src={plus} alt="Add Project" />}
+      <img src={profile} alt="Profile" />
+      <img src={settings} alt="settings" />
+      <img src={home} alt="home" />
+      <img onClick={handleCreateProject} src={plus} alt="Add Project" />
+      <img onClick={handleSignIn} src={signIn} alt="Sign In" />
     </nav>
   );
 };
