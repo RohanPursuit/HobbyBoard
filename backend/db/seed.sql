f19e8462c6a2e12bc2162603e6381c55eb12f8af
BennyZ3 <bennyz3@hotmail.com>
2022-05-17 11:50:38 -0400

    seed update

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -3,12 +3,12 @@
 INSERT INTO testTable(content) VALUES
 ('This is our database''s test data');
 
-INSERT INTO projects(name, details, project_image, archived) VALUES
-('T -1Hour','This is an idea for a video game that begins about an hour before the downfall of society..', null, false),
-('Rohan''s Gamer Dating App', 'something something games', null, false),
-('Noob game devs', 'Help me make a game.', null, false),
-('Backflippers', 'and we do be hittin backflips 4real', null, false),
-('Legal Drive', 'An illegal racing game that takes place in New York city, where racers can coordinate street racing events using an underground App. This App will track winners and transfer money to participants.', null, false);
+INSERT INTO projects(name, details, project_image, archived, creators) VALUES
+('T -1Hour','This is an idea for a video game that begins about an hour before the downfall of society..', null, false, 'RS'),
+('Rohan''s Gamer Dating App', 'something something games', null, false, 'RS'),
+('Noob game devs', 'Help me make a game.', null, false, 'JD'),
+('Backflippers', 'and we do be hittin backflips 4real', null, false, 'DL'),
+('Legal Drive', 'An illegal racing game that takes place in New York city, where racers can coordinate street racing events using an underground App. This App will track winners and transfer money to participants.', null, false, 'BZ');
 
 
 
@@ -22,6 +22,7 @@ INSERT INTO connections (username, project_id, permissions) VALUES
 ('BZ', 5, 'owner'),
 ('JD', 3, 'owner'),
 ('RS', 1, 'owner'),
-('DL', 4, 'owner');
+('DL', 4, 'owner'),
+('RS', 2, 'owner');
 -- ('BZ', 4, 'collaborator'),
 -- ('BZ', 3, 'follower');
