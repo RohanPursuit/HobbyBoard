60684b7988290ff8098bff6ced3c7374f1b69dbf
DylanLovell93 <arcanyne7@gmail.com>
2022-05-19 17:58:18 -0400

    default image helper function

---
+++ frontend/src/helpers/helperFunction.js
@@ -0,0 +1,8 @@
+// function for default image;
+const defaultImage = (event) => {
+  event.target.src =
+    "https://redzonekickboxing.com/wp-content/uploads/2017/04/default-image.jpg";
+};
+
+//export helper function
+export default { defaultImage };
