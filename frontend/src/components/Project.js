cbd38ccf74a47ac70d276eb1087a5279f4efd96b
DylanLovell93 <arcanyne7@gmail.com>
2022-05-19 19:06:33 -0400

    project.js default img

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,18 +1,12 @@
 import { Link } from "react-router-dom";
-import { Img } from "react-image";
+import defaultImage from "../helpers/helperFunction";
 import "./Project.css";
 
 const Project = (props) => {
   return (
     <Link to={`/projects/${props.details.project_id}`}>
       <div className="Project">
-        <Img
-          src={[
-            props.project_image,
-            "https://redzonekickboxing.com/wp-content/uploads/2017/04/default-image.jpg",
-          ]}
-          alt=""
-        />
+        <img src={props.project_image || ""} alt="" onError={defaultImage} />
         <h3>{props.details.name}</h3>
         <p>{props.details.details}</p>
       </div>
