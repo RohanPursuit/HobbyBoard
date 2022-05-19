817459f70ee9558c1989bdb885dc4a5842e00dff
justin <justin.d.diaz@gmail.com>
2022-05-19 12:15:54 -0400

    editProfile working

--- frontend/src/components/ProfileEdit.js
+++ frontend/src/components/ProfileEdit.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const ProfileEdit = () => {
-  const { pid } = useParams();
   const navigator = useNavigate();
   const API = process.env.REACT_APP_API_URL;
   const [user, setUser] = useState({
@@ -14,25 +13,25 @@ const ProfileEdit = () => {
   });
 
   useEffect(() => {
-    axios.get(`${API}/profile`).then((response) => setUser(response.data));
-  }, [API, pid]);
+    axios.get(`${API}users/JD`).then((response) => setUser(response.data));
+  }, [API]);
 
   const handleChange = (event) => {
     setUser({
       ...user,
-      [event.target.pid]: event.target.value,
+      [event.target.id]: event.target.value,
     });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.put(`${API}/profile`, user).then(() => navigator(`/profile`));
+    axios.put(`${API}users/JD`, user).then(() => navigator(`/profile`));
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="username">username:</label>
+        <label htmlFor="username">Username:</label>
         <input
           id="username"
           type="text"
@@ -41,7 +40,7 @@ const ProfileEdit = () => {
           required
         />
         <label htmlFor="password">Password:</label>
-        <textarea
+        <input
           id="password"
           type="text"
           value={user.password}
