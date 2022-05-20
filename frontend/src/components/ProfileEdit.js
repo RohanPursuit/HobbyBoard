ff1ae3435632e8e349adf680d69f68301f7c6133
RohanPursuit <rohansterling@pursuit.org>
2022-05-20 07:05:15 -0400

    import ProfileEdit.css

--- frontend/src/components/ProfileEdit.js
+++ frontend/src/components/ProfileEdit.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import './ProfileEdit.css'
 
 const ProfileEdit = ({ username }) => {
   const navigator = useNavigate();
