efe2e2e8e4ed9b85c59c22d050ce446e5b0e5012
RohanPursuit <rohansterling@pursuit.org>
2022-05-20 07:03:50 -0400

    add className EditProfile

--- frontend/src/pages/EditP.js
+++ frontend/src/pages/EditP.js
@@ -3,7 +3,7 @@ import ProfileEdit from "../components/ProfileEdit";
 const EditP = () => {
   const user = document.cookie.split("=")[1];
   return (
-    <div>
+    <div className="EditProfile">
       <ProfileEdit username={user} />
     </div>
   );
