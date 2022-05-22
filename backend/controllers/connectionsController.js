c39b47a1a6a308bcb0a1190e7f9cd2e742006643
DylanLovell93 <arcanyne7@gmail.com>
2022-05-22 14:54:33 -0400

    created connections controller, added .rest to gitignore for express route testing

--- backend/controllers/connectionsController.js
+++ backend/controllers/connectionsController.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const connections = express();
-const { joinRequest, deleteRequest } = require("../queries/connectionsQueries");
+const {
+  joinRequest,
+  deleteRequest,
+  getAllProjectConnections,
+} = require("../queries/connectionsQueries");
 
 //Send join request
 connections.post("/", async (request, response) => {
@@ -13,6 +17,12 @@ connections.delete("/", async (request, response) => {
   console.log("delete /connections");
   const removeConnection = await deleteRequest(request.body);
   response.status(200).json(removeConnection);
+});
+
+connections.get("/", async (request, response) => {
+  console.log("get /connections");
+  const projectConnections = await getAllProjectConnections(request.body);
+  response.status(200).json(projectConnections);
 });
 
 module.exports = connections;
