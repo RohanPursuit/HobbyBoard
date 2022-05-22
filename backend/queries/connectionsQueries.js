b917aa596dfc7180471ef03a6fe0bf5b4c298b5f
justin <justin.d.diaz@gmail.com>
2022-05-22 12:06:52 -0400

    delete

--- backend/queries/connectionsQueries.js
+++ backend/queries/connectionsQueries.js
@@ -5,16 +5,32 @@ const db = require("../db/dbConfig");
 //input(requestInfo) requestInfo {username: "", project_id: ""}
 //output inserted row
 const joinRequest = async (requestInfo) => {
- const { username, project_id } = requestInfo
- //try to create new join request to project_id
-    try{
-        const pending = await db.one("INSERT INTO connections (username, project_id, permissions) VALUES ($1, $2, $3) RETURNING *", [username, project_id, "request"])
-        //return inserted row
-        return pending
-    } catch(err){
-        //if err, return err
-        return err
-    }
-}
+  const { username, project_id } = requestInfo;
+  //try to create new join request to project_id
+  try {
+    const pending = await db.one(
+      "INSERT INTO connections (username, project_id, permissions) VALUES ($1, $2, $3) RETURNING *",
+      [username, project_id, "request"]
+    );
+    //return inserted row
+    return pending;
+  } catch (err) {
+    //if err, return err
+    return err;
+  }
+};
 
-module.exports = joinRequest
+const deleteRequest = async (id) => {
+  try {
+    const removeRequest = await db.one(
+      "DELETE FROM connections WHERE (username, project_id, permissions) VALUES ($1, $2, $3) RETURNING *",
+      id
+    );
+
+    return removeRequest;
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = { joinRequest, deleteRequest };
