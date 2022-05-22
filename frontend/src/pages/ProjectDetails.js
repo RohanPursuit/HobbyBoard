f48e26008122ab0cf1e746dc1ff041b24efa5efe
RohanPursuit <rohansterling@pursuit.org>
2022-05-22 11:20:28 -0400

    add axois post request, sending join request

--- frontend/src/pages/ProjectDetails.js
+++ frontend/src/pages/ProjectDetails.js
@@ -36,6 +36,19 @@ const ProjectDetails = () => {
     nav("/profile/" + project.creator);
   };
 
+  const handleJoin = () => {
+    axios.post(`${API}connections`, {
+      username: document.cookie.split("=")[1],
+      project_id: project.project_id
+    })
+    .then(() => {
+      alert("Request Pending")
+    })
+    .catch(() =>{
+      alert("Request failed")
+    })
+  }
+
   return (
     <div className="ProjectDetails">
       <img
@@ -65,7 +78,7 @@ const ProjectDetails = () => {
                 return <a href={link}>{link}</a>
             })} */}
       {/* Contributors */}
-      {document.cookie.split("=")[1] !== project.creator ? <button>Join</button>
+      {document.cookie.split("=")[1] !== project.creator ? <button onClick={handleJoin}>Join</button>
       :
       ""}
     </div>
