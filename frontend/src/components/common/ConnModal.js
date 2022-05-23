6ac6a167d58a74bb4e391e023b7354d45599438d
DylanLovell93 <arcanyne7@gmail.com>
2022-05-23 18:03:35 -0400

    positioning and rendering for example modal

--- frontend/src/components/common/ConnModal.js
+++ frontend/src/components/common/ConnModal.js
@@ -4,8 +4,8 @@ import "./ConnModal.css";
 const ConnModal = ({ display, project_id, setDisplay }) => {
   return (
     <div className="ConnModal">
-      ConnModal
       <div className="darkScreen" onClick={setDisplay}></div>
+      <div className="modalContent">Example Modal</div>
     </div>
   );
 };
