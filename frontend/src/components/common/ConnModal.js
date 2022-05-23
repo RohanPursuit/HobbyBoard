24ce870db73810307c1c05ec3761e808d3b20788
DylanLovell93 <arcanyne7@gmail.com>
2022-05-23 12:34:01 -0400

    creation of Modal div & css files

---
+++ frontend/src/components/common/ConnModal.js
@@ -0,0 +1,8 @@
+import "./ConnModal.css";
+
+//input {display Status (boolnean), collaborators(array), request(array)}
+const ConnModal = ({ displayStatus, collaborators, requests }) => {
+  return <div className="ConnModal">ConnModal</div>;
+};
+
+export default ConnModal;
