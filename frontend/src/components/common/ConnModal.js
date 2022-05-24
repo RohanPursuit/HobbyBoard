bf678dd1b1d55ef0e370dca5129f4910b56cfe12
DylanLovell93 <arcanyne7@gmail.com>
2022-05-24 11:17:55 -0400

    seperate request from collaborators and owners

--- frontend/src/components/common/ConnModal.js
+++ frontend/src/components/common/ConnModal.js
@@ -5,6 +5,11 @@ import axios from "axios";
 //input project_id(number), setDisplay(state function)}
 const ConnModal = ({ project_id, setDisplay }) => {
   const [conns, setConns] = useState([]);
+  const collaborators = conns.filter(
+    (e) => e.permissions === "collaborator" || e.permissions === "owner"
+  );
+  const requesters = conns.filter((e) => e.permissions === "request");
+  console.log(collaborators, requesters);
   const URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
