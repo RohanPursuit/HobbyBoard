d0a5b4a3bccf7ac158c28f89b5b6ff306478c8de
DylanLovell93 <arcanyne7@gmail.com>
2022-05-24 12:02:33 -0400

    hoising helper function added and used

--- frontend/src/helpers/helperFunction.js
+++ frontend/src/helpers/helperFunction.js
@@ -3,3 +3,13 @@ export default function defaultImage(event) {
   event.target.src =
     "https://redzonekickboxing.com/wp-content/uploads/2017/04/default-image.jpg";
 }
+
+//function for hoisting collaborator to the top
+export function hoistCollabCard(collabs) {
+  const user = document.cookie.split("=")[1];
+  const userPosition = collabs.findIndex((e) => e.username === user);
+  // if the user doesn't exist in our array, return the original array, else hoist their name to the top.
+  return userPosition === -1
+    ? collabs
+    : collabs.splice(userPosition, 1).concat(collabs);
+}
