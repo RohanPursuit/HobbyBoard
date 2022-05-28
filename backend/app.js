195c337b535856894d8f77606319cd31e81c430b
RohanPursuit <rohansterling@pursuit.org>
2022-05-28 11:44:56 -0400

    require http and socket.io, createServer using app, include env FRONT_END_URL in cors origin array of allowed connections, setup connections event, add notify middleware and notify functions

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
+const http = require('http')
 
 //IMPORT QUERIES
 const { testQuery } = require("./queries/testQuery");
@@ -11,6 +12,16 @@ const connectionsController = require("./controllers/connectionsController")
 // CONFIGURATION
 const app = express();
 
+const server = http.createServer(app)
+const io = require('socket.io')(server, {
+  cors:{
+      origin: [process.env.FRONT_END_URL]
+  }})
+
+io.on('connection', socket => {
+  console.log("Hi", socket.id)
+})
+
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json()); // Parse incoming JSON
@@ -19,6 +30,15 @@ app.use(express.json()); // Parse incoming JSON
 app.use("/projects", projectsControllers);
 app.use("/users", userController);
 app.use("/connections", connectionsController)
+app.use("/connections", notify)
+
+function notify(req,res){
+  const {project_id} = req.body
+  if(req.method === "POST" || req.method === "DELETE"){
+    io.emit("request" + project_id, "New Request") //rename
+    console.log("New/DELETE Request")
+  }
+}
 
 //async, so we can use query correctly
 app.get("/", async (req, res) => {
@@ -28,4 +48,4 @@ app.get("/", async (req, res) => {
 });
 
 // EXPORT
-module.exports = app;
+module.exports = server;
