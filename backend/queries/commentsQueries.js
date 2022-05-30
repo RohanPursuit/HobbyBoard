2cb0ac9c96f564be08446887be364fe15e57ac2e
DylanLovell93 <arcanyne7@gmail.com>
2022-05-30 01:27:33 -0400

    createComment query

--- backend/queries/commentsQueries.js
+++ backend/queries/commentsQueries.js
@@ -1,11 +1,11 @@
 //import DB
-const db = require('../db/dbConfig');
+const db = require("../db/dbConfig");
 
 //get all comments from a post
 const getAllComments = async ({ post_id }) => {
   try {
     const allComments = await db.any(
-      'SELECT * FROM comments WHERE post_id=$1',
+      "SELECT * FROM comments WHERE post_id=$1",
       post_id
     );
     return allComments;
@@ -14,4 +14,17 @@ const getAllComments = async ({ post_id }) => {
   }
 };
 
-module.exports = { getAllComments };
+// create new comment
+const createComment = async ({ post_id, username, comment, date }) => {
+  try {
+    const newComment = await db.one(
+      "INSERT INTO comments (post_id, username, comment, date) VALUES ($1, $2, $3, $4) RETURNING *",
+      [post_id, username, comment, date]
+    );
+    return newComment;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllComments, createComment };
