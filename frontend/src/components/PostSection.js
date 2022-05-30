f6fa9f17362b4a1ffe18b835e34ea98f0826c441
DylanLovell93 <arcanyne7@gmail.com>
2022-05-30 18:01:52 -0400

    render newPost button

--- frontend/src/components/PostSection.js
+++ frontend/src/components/PostSection.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./PostSection.css";
 
 const PostSection = () => {
-  return <div className="PostSection">PostSection</div>;
+  return (
+    <div className="PostSection">
+      <button className="newPost">New Post</button>
+    </div>
+  );
 };
 
 export default PostSection;
