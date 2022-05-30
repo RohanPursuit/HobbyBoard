4a63706d3a8c843762f04c4aca93c3a48c4c3192
DylanLovell93 <arcanyne7@gmail.com>
2022-05-30 07:00:12 -0400

    delete comment route

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -5,21 +5,26 @@ const express = require("express");
 const posts = express.Router();
 
 //import queries
-const { getAllComments, createComment } = require("../queries/commentsQueries");
+const {
+  getAllComments,
+  createComment,
+  deleteComment,
+} = require("../queries/commentsQueries");
 
 //create routes
 
 // getAllcomments from post
 
 posts.get("/:post_id/comments", async (request, response) => {
-  console.log(`get request to /${request.params.post_id}/comments`);
+  console.log(`get request to posts/${request.params.post_id}/comments`);
   const allComments = await getAllComments(request.params);
   response.status(200).json(allComments);
 });
 
 // create newComment
+
 posts.post("/:post_id/comments", async (request, response) => {
-  console.log(`post request to /${request.params.post_id}/comments`);
+  console.log(`post request to /posts/${request.params.post_id}/comments`);
   // get post_id from params
   const { post_id } = request.params;
   // get username & comment from body
@@ -30,6 +35,15 @@ posts.post("/:post_id/comments", async (request, response) => {
   const newComment = await createComment(post_id, username, comment, date);
   //send the result to the user
   response.status(200).json(newComment);
+});
+
+//delete a comment
+posts.delete("/:post_id/comments/:comment_id", async (request, response) => {
+  console.log(
+    `delete request to /posts/${request.params.post_id}/comments/${request.params.comment_id}`
+  );
+  const deletedComment = await deleteComment(request.params);
+  response.status(200).json(deletedComment);
 });
 
 //export router
