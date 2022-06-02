42a9797c3d369ce0d0e39c21f9ac9bc52855cf34
RohanPursuit <rohansterling@pursuit.org>
2022-06-02 13:24:44 -0400

    add svg.js, testing css stroke and fill rules

---
+++ frontend/src/assets/svgs.js
@@ -0,0 +1,6 @@
+const arrowBackCircle = <svg className="arrow-back-circle" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.79313 10.5L5.3125 8L7.79313 5.5M5.65719 8H10.6875" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+
+
+export default arrowBackCircle
