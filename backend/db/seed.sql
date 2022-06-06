054d7cf75a69d7de59cecb495fbb5fa2ddded315
BennyZ3 <bennyz3@hotmail.com>
2022-06-06 00:59:09 -0400

    updated image for BZ profile and legal drive

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -4,7 +4,7 @@ INSERT INTO testTable(content) VALUES
 ('This is our database''s test data');
 
 INSERT INTO users(username, password, email, date, details, created_projects, collaborated_projects, profile_image) VALUES 
-('BZ', 'password', 'email@email.com', '05/13/2022', 'Can type gud.','{Legal Drive}','{Noob game devs}', null),
+('BZ', 'password', 'email@email.com', '05/13/2022', 'Can type gud.','{Legal Drive}','{Noob game devs}', 'https://drive.google.com/uc?id=1xNU72FBvkrAmhZTtKbqPMTm-yPuU3CUx&export=download'),
 ('JD', 'password', 'email@email.com', '05/14/2022', 'Can type gud.','{Noob game devs}', '{}', null),
 ('RS', 'password', 'email@email.com', '05/15/2022', 'Can type gudder.','{T -1Hour,Rohan''s Gamer Dating App}', '{}', null),
 ('DL', 'password', 'email@email.com', '05/16/2022', 'Can type gud.','{Backflippers}', '{}', null);
@@ -15,7 +15,7 @@ INSERT INTO projects(name, details, project_image, archived, creator) VALUES
 ('Rohan''s Gamer Dating App', 'something something games', null, false, 'RS'),
 ('Noob game devs', 'Help me make a game.', null, false, 'JD'),
 ('Backflippers', 'and we do be hittin backflips 4real', null, false, 'DL'),
-('Legal Drive', 'An illegal racing game that takes place in New York city, where racers can coordinate street racing events using an underground App. This App will track winners and transfer money to participants.', null, false, 'BZ');
+('Legal Drive', 'An illegal racing game that takes place in New York city, where racers can coordinate street racing events using an underground App. This App will track winners and transfer money to participants.', 'https://www.harlemworldmagazine.com/wp-content/uploads/2020/09/The-Fate-Of-The-Furious-64-f3.jpg', false, 'BZ');
 
 INSERT INTO connections (username, project_id, permissions) VALUES
 ('BZ', 5, 'owner'),
