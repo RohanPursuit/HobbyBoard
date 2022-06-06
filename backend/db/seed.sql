c3b903e50b6ac7703ed2faefbabf8b6d962a85df
DylanLovell93 <arcanyne7@gmail.com>
2022-06-06 03:16:06 -0400

    merge fixes

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -15,19 +15,20 @@ INSERT INTO projects(name, details, project_image, archived, creator) VALUES
 ('Reset','With the project Reset I would like to explore the idea of an apocalyptic video game that begins about a day or two before the apocalypse. Reset is an open world game with a single player/co-op mode. However for this project I will be focusing on making a playable demo of the single player/co-op experience. My main motivation for creating a game like Reset is exploring player decision making during the initial events at the beginning of the apocalypse.', 'https://static.onecms.io/wp-content/uploads/sites/28/2021/02/19/new-york-city-evening-NYCTG0221.jpg', false, 'RS'),
 ('Noob game devs', 'Hello, I''m look for others who would like to learn about the process of developing games, to play and understand game design, and to collaborate with others in researching and creating our own games.', 'https://i.imgur.com/s5n9x1q.jpg', false, 'JD'),
 ('CompatABLE', 'An idea for creating a website that can be used to find compatability between two different pieces of technology, be it hardware or software', 'https://www.invensis.net/blog/wp-content/uploads/2015/06/Compatibility-Testing-Invensis.jpg', false, 'DylanL'),
-('Rohan''s Gamer Dating App', 'something something games', null, false, 'RS'),
 ('Legal Drive', 'An illegal racing game that takes place in New York city, where racers can coordinate street racing events using an underground App. This App will track winners and transfer money to participants.', 'https://www.harlemworldmagazine.com/wp-content/uploads/2020/09/The-Fate-Of-The-Furious-64-f3.jpg', false, 'BZ');
+--('Rohan''s Gamer Dating App', 'something something games', null, false, 'RS'),
 
 INSERT INTO connections (username, project_id, permissions) VALUES
-('BZ', 5, 'owner'),
-('JD', 2, 'owner'),
 ('RS', 1, 'owner'),
-('RS', 4, 'owner'),
+('JD', 2, 'owner'),
 ('DylanL', 3, 'owner'),
+('BZ', 4, 'owner'),
+('BZ', 2, 'collaborator'),
+('BZ', 2, 'follow'),
 ('BZ', 3, 'collaborator'),
-('BZ', 4, 'request'),
+('BZ', 3, 'request'),
+('JD', 3, 'collaborator'),
 ('BZ', 4, 'follower'),
-('JD', 4, 'collaborator'),
 ('RS', 4, 'collaborator');
 
 
@@ -35,10 +36,10 @@ INSERT INTO posts (project_id, members_only, date, title, contents, likes) VALUE
 (1, false, '2020-9-04 10:23:54', 'Character model: Lisa', 'We just finished the first charater model. Check it out HERE', ARRAY ['RS', 'BZ','JD','DylanL']),
 (2, true, '2020-9-05 10:23:54', 'Post 2', 'This is our second test post', ARRAY ['']),
 (2, false, '2020-9-06 10:23:54', 'Post 3', 'This is our third test post', ARRAY ['']),
-(3, false, '2020-9-07 10:23:54', 'Post 4', 'This is our fourth test post', ARRAY ['']),
-(5, false, '2020-9-08 10:23:54', 'Post 5', 'This is our fifth test post', ARRAY ['']),
-(4, true, '2022-3-04 10:23:54', 'Post 5', 'Discord: -here-, Facebook: -here-', ARRAY ['']),
-(4, false, '2022-5-07 10:23:54', 'Post 5', 'We''re in Alpha! Looking for members to help with video editing! ', ARRAY ['', 'JD', 'BZ']);
+(2, false, '2020-9-07 10:23:54', 'Post 4', 'This is our fourth test post', ARRAY ['']),
+(3, true, '2022-3-04 10:23:54', 'Post 5', 'Discord: -here-, Facebook: -here-', ARRAY ['']),
+(3, false, '2022-5-07 10:23:54', 'Post 5', 'We''re in Alpha! Looking for members to help with video editing! ', ARRAY ['', 'JD', 'BZ']),
+(4, false, '2020-9-08 10:23:54', 'Post 5', 'This is our fifth test post', ARRAY ['']);
 
 INSERT INTO comments (post_id, username, comment, date) VALUES 
 (1, 'DylanL', 'Wow, this is amazing', '2020-9-05 10:23:54'),
