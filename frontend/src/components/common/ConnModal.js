0462ce09bfd77133c9aa74cbdb608fef9e255565
RohanPursuit <rohansterling@pursuit.org>
2022-06-09 16:09:37 -0400

    add dependencies array to useCallback

--- frontend/src/components/common/ConnModal.js
+++ frontend/src/components/common/ConnModal.js
@@ -22,8 +22,7 @@ const ConnModal = ({ project_id, setDisplay, owner, pageReload }) => {
   const listenForRequests = useCallback(() => {
     socket.off().on("request" + project_id, modalReload);
     console.log("ran");
-  }
-  )
+  }, [URL])
 
   useEffect(() => {
     axios
